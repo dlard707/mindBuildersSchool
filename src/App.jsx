1a2827eb97055bbaeb89dba306023843db53a783
dlard707 <dlard707@yahoo.com.ar>
2022-08-21 14:44:04 -0300

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     
       <h1>MindBuilders</h1>
       <div className="card">
-        <button >
-          Contato
-        </button>
-        <p>
-        Ensinando habilidades reais para o mundo de hoje
-        </p>
+        <button>Contato</button>
+        <p>Ensinando habilidades reais para o mundo de hoje</p>
       </div>
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
